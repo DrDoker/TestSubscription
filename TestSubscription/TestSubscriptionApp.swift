7c3c8a5d57a2d0f55ca21feb4c51ff4195c5360c
Serhii Tkachenko <doker.tk@gmail.com>
2024-10-02 21:19:09 +0200

    Initial Commit

---
+++ TestSubscription/TestSubscriptionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSubscriptionApp.swift
+//  TestSubscription
+//
+//  Created by Serhii on 02.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestSubscriptionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
